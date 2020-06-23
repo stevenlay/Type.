e3c7c810dd7b8b4e064903e166e7953d20478cab
stevenlay <sl@gatech.edu>
2020-06-22 23:54:44 -0700

    character typing logic

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   StyledGame,
   StyledScore,
@@ -8,14 +8,17 @@ import {
 import { Strong } from '../styled/Random';
 
 export default function Game({ history }) {
-  const MAX_SECONDS = 5;
+  const MAX_SECONDS = 10;
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const [currentCharacter, setCurrentCharacter] = useState('');
   const [score, setScore] = useState(0);
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
-    const interval = setInterval(() => updateTime(currentTime), 105);
+    const interval = setInterval(() => updateTime(currentTime), 65);
     return () => clearInterval(interval);
   }, []);
 
@@ -47,12 +50,38 @@ export default function Game({ history }) {
     }
   }, [seconds, ms, history]);
 
+  const keyUpHandler = useCallback(
+    e => {
+      if (e.key === currentCharacter) {
+        setScore(prevScore => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore(prevScore => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },
+    [currentCharacter]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keyup', keyUpHandler);
+    return () => {
+      document.removeEventListener('keyup', keyUpHandler);
+    };
+  }, [keyUpHandler]);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
+  };
+
   return (
     <StyledGame>
       <StyledScore>
         Score: <Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
         Time: {seconds} : {ms}
       </StyledTimer>

e1f6acac09fb3aa61c0573138632cd897794b893
stevenlay <sl@gatech.edu>
2020-06-24 18:54:01 -0700

    high score listing

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ScoresList, ScoresLI } from '../styled/HighScores';
 
 export default function HighScores() {
+  const [highScores, setHighScores] = useState([]);
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        console.log('fetching');
+        const res = await fetch('/.netlify/functions/GetHighScores');
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {}
+    };
+    loadHighScores();
+  }, []);
   return (
     <div>
-      <h1>HighScores</h1>
+      <h1>High Scores</h1>
+      <ScoresList>
+        {highScores.map(({ fields }, index) => (
+          <ScoresLI key={index}>
+            {index + 1}. {fields.name} - {fields.score}
+          </ScoresLI>
+        ))}
+      </ScoresList>
     </div>
   );
 }
---
+++ src/styled/HighScores.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ScoresList = styled.ol`
+  padding-left: 0;
+  list-style: none;
+`;
+
+export const ScoresLI = styled.li`
+  font-size: 1.5rem;
+  margin-bottom: 0.8rem;
+`;
